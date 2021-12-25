3a209af7011c4dd478016be2ab0d7121fdf3a9fc
rhythm191 <sac2ndg+develop@gmail.com>
2021-12-25 11:49:56 +0900

    feat: add main fragment

---
+++ app/src/main/java/net/rhyztech/android/hanasukotonai/MainApplication.kt
@@ -0,0 +1,8 @@
+package net.rhyztech.android.hanasukotonai
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application() {
+}
